be372cc75a399eecabf29df7d71cefe86dd1bf12
utsha das <utshachandradas@gmail.com>
2023-09-27 19:00:04 +0600

    useState learning path

--- Day12/learn-four/src/App.jsx
+++ Day12/learn-four/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [myObj, setObj] = useState({
+    key1: "I am key one",
+    key2: "I am key two",
+    key3: "I am key three",
+  });
+  const change = () => {
+    setObj((prevObj) => ({
+      ...prevObj,
+      key1: "I am new key one",
+      key2: "I am new key two",
+    }));
+  };
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>{myObj.key1}</h1>
+        <h2>{myObj.key2}</h2>
+        <h3>{myObj.key3}</h3>
+        <button onClick={change}>Change</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
