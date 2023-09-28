8ace1ddbbce65664c350a193bca68f21b515ae22
utsha das <utshachandradas@gmail.com>
2023-09-28 14:46:46 +0600

    todo App using useState

---
+++ Projects/todo-app/src/App.jsx
@@ -0,0 +1,59 @@
+/* eslint-disable react/jsx-key */
+import { useState } from "react";
+
+import "./assets/css/todo.css";
+function App() {
+  const [list, setList] = useState([]);
+  const [item, setItem] = useState();
+  const todoList = () => {
+    list.push(item);
+    setList([...list]);
+  };
+
+  const removeItem = (index) => {
+    // alert(index);
+    list.splice(index, 1);
+    setList([...list]);
+  };
+  return (
+    <>
+      <div className="container">
+        <input
+          type="text"
+          onChange={(e) => {
+            setItem(e.target.value);
+          }}
+          placeholder="Add your task"
+        />
+        <button onClick={todoList}> ADD</button>
+        <table>
+          <tbody>
+            {list.length !== 0 ? (
+              list.map((element, index) => {
+                return (
+                  <tr>
+                    <td>{element}</td>
+                    <td>
+                      <button
+                        className="remove"
+                        onClick={() => {
+                          removeItem(index);
+                        }}
+                      >
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr></tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default App;
